fe2a55d918b54057ade01431cd126558ba07e51d
Mike Bloy <mike@bloy.org>
2015-12-04 08:28:55 -0600

    day3.2

--- dec3.py
+++ dec3.py
@@ -5,20 +5,25 @@ with open('input/day_3') as file:
     data = file.read()
 
 houses = collections.Counter()
-position_x = 0
-position_y = 0
-houses[(0,0)] = 1
+position_x = [0, 0]
+position_y = [0, 0]
+houses[(0,0)] = 2
+santa = 0
 for direction in data.strip():
     if direction == '^':
-        position_y += 1
+        position_y[santa] += 1
     elif direction == 'v':
-        position_y -= 1
+        position_y[santa] -= 1
     elif direction == '>':
-        position_x += 1
+        position_x[santa] += 1
     elif direction == '<':
-        position_x -= 1
+        position_x[santa] -= 1
     else:
         print('unknown character "{0}"'.format(direction))
-    houses[(position_x, position_y)] += 1
+    houses[(position_x[santa], position_y[santa])] += 1
+    if santa == 1:
+        santa = 0
+    else:
+        santa = 1
 
 print(len(houses.keys()))
