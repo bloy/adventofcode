e3b8c5bc52d36b6b958b8be30604debdc5b50383
Mike Bloy <mike@bloy.org>
2015-12-05 10:08:58 -0600

    completed day 4.2

--- dec4.py
+++ dec4.py
@@ -5,7 +5,7 @@ import hashlib
 secret = 'iwrupvqb'
 number = -1
 hash_result = 'xxxxxxxxxxxxxxxxxxx'
-while hash_result[:5] != '00000':
+while hash_result[:6] != '000000':
     number = number + 1
     secret_string = secret + str(number)
     hash_result = hashlib.md5(secret_string.encode('utf-8')).hexdigest()
