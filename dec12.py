a752fe9a78bec0e792e4130ec0d9ed95be651900
Mike Bloy <mike@bloy.org>
2015-12-11 23:59:05 -0600

    day 12 part 2

--- dec12.py
+++ dec12.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import re
+import json
 
 
 def find_numbers(in_data):
@@ -9,8 +10,22 @@ def find_numbers(in_data):
     print(numbers)
     return numbers
 
+def no_red_accounting(data):
+    if isinstance(data, int):
+        return data
+    if isinstance(data, list):
+        return sum(no_red_accounting(value) for value in data)
+    if isinstance(data, dict):
+        if any(data[key] == 'red' for key in data.keys()):
+            return 0
+        return sum(no_red_accounting(data[key]) for key in data)
+    return 0
+
 
 if __name__ == '__main__':
     with open('input/day_12') as infile:
         print(sum(find_numbers(infile)))
 
+    with open('input/day_12') as infile:
+        data = json.load(infile)
+        print(no_red_accounting(data))
