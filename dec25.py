593a798fcfca8a921dc965cc5df94ff0c5423cdf
Mike Bloy <mike@bloy.org>
2015-12-24 23:44:37 -0600

    dec25 brute force all the way

--- dec25.py
+++ dec25.py
@@ -3,12 +3,18 @@
 FIRST_VALUE = 20151125
 INPUT_COLUMN = 3083
 INPUT_ROW = 2978
+MAGIC_MULTIPLIER = 252533
+MAGIC_MOD = 33554393
 
 def sequence_number(row, column):
     return sum(range(row + column - 1)) + column
 
+def next_code(code):
+    return (code * MAGIC_MULTIPLIER) % MAGIC_MOD
 
 if __name__ == '__main__':
-    for x in range(1, 7):
-        for y in range(1, 8 - x):
-            print(x, y, sequence_number(x, y))
+    code_sequence_number = sequence_number(INPUT_ROW, INPUT_COLUMN)
+    current_code = FIRST_VALUE
+    for i in range(code_sequence_number - 1):
+        current_code = next_code(current_code)
+    print(current_code)
