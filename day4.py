dee7b694e1a72d9667bfc809cd5562e47f330bfa
Mike Bloy <mike@bloy.org>
2016-12-04 10:18:19 -0600

    solve day4 part 1 and 2

--- day4.py
+++ day4.py
@@ -6,6 +6,7 @@ import itertools
 import pprint
 import re
 
+
 class Room(collections.namedtuple('Room', 'name sector checksum')):
     __slots__ = ()
     roomexp = re.compile(
@@ -30,24 +31,37 @@ class Room(collections.namedtuple('Room', 'name sector checksum')):
     def valid(self):
         return self.computed_checksum == self.checksum
 
+    def shift_letter(self, letter):
+        if letter == '-':
+            return ' '
+        return chr((ord(letter) - ord('a') + int(self.sector)) % 26 + ord('a'))
+
+    @property
+    def decrypted_name(self):
+        return "".join(self.shift_letter(letter) for letter in self.name)
+
 
 def solve1(data):
     rooms = (Room.unpack(item) for item in data)
     return sum(int(room.sector) for room in rooms if room.valid)
 
 
-
 def solve2(data):
     rooms = (Room.unpack(item) for item in data)
+    return [room for room in rooms
+            if room.valid and room.decrypted_name == 'northpole object storage']
 
 
 if __name__ == '__main__':
-    data = [
-        'aaaaa-bbb-z-y-x-123[abxyz]',
-        'a-b-c-d-e-f-g-h-987[abcde]',
-        'not-a-real-room-404[oarel]',
-        'totally-a-real-room-200[decoy]',
-    ]
+    # data = [
+        # 'aaaaa-bbb-z-y-x-123[abxyz]',
+        # 'a-b-c-d-e-f-g-h-987[abcde]',
+        # 'not-a-real-room-404[oarel]',
+        # 'totally-a-real-room-200[decoy]',
+    # ]
+    with open('day4_input.txt') as f:
+        data = [line for line in f.read().split("\n") if line]
+    data = [d for d in data if d]
 
-    print(solve1(data))
-    print(solve2(data))
+    pprint.pprint(solve1(data))
+    pprint.pprint(solve2(data))
