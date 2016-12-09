3504319674db56a8ad0ce0bb3f4da8486fc24d4d
Mike Bloy <mike@bloy.org>
2016-12-09 07:30:49 -0600

    day9 prep

---
+++ day9.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import pprint
+import re
+
+
+def solve1(data):
+    return data
+
+
+def solve2(data):
+    return data
+
+
+if __name__ == '__main__':
+    with open('day9_input.txt') as f:
+        data = f.read()
+
+    print(solve1(data))
+    print(solve2(data))
