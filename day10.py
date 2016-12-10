a2dfb389ff9135c8d563e6b284a5e180c1b5f31e
Mike Bloy <mike@bloy.org>
2016-12-09 23:00:10 -0600

    day 10 prep

---
+++ day10.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import pprint
+import re
+
+
+def solve1(data):
+    pass
+
+
+def solve2(data):
+    pass
+
+
+if __name__ == '__main__':
+    with open('day9_input.txt') as f:
+        data = [line.strip() for line in f]
+
+    print(solve1(data))
+    print(solve2(data))
