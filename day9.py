f48d6f2932f46c16ca4cea8d04a7cd5e7cf325c5
Mike Bloy <mike@bloy.org>
2016-12-09 23:00:00 -0600

    day 9 part 2

--- day9.py
+++ day9.py
@@ -20,15 +20,28 @@ def solve1(data):
     return len(decompressed)
 
 
+def decompressed_length(compressed):
+    decom_len = 0
+    split = re.split(COMPRESSION_REGEX, compressed, maxsplit=1)
+    while split[0] != compressed:
+        decom_len += len(split[0])
+        (run, count, compressed) = (int(split[1]), int(split[2]), split[3])
+        decom_len += (decompressed_length(compressed[:run]) * count)
+        compressed = compressed[run:]
+        split = re.split(COMPRESSION_REGEX, compressed, maxsplit=1)
+    decom_len += len(split[0])
+    return decom_len
+
+
 def solve2(data):
-    return 0
+    return decompressed_length(data)
 
 
 if __name__ == '__main__':
     with open('day9_input.txt') as f:
         data = f.read().strip()
 
-    # data = 'X(8x2)(3x3)AB(2x2)CY'
+    # data = '(27x12)(20x12)(13x14)(7x10)(1x12)A'
 
     print(solve1(data))
     print(solve2(data))
