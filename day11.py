951634d7022603782025f90d4289447c5f869205
Mike Bloy <mike@bloy.org>
2016-12-10 22:53:48 -0600

    day 11 prep

---
+++ day11.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import collections
+import pprint
+import re
+
+
+def solve1(data):
+    return 0
+
+
+def solve2(data):
+    return 0
+
+
+if __name__ == '__main__':
+    with open('day11_input.txt') as f:
+        data = [line.strip() for line in f]
+
+
+    pprint.pprint(solve1(data))
+    pprint.pprint(solve2(data))
