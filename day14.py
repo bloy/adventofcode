6d4d2689d89465c6d0646a91d081a73950b52d07
Mike Bloy <mike@bloy.org>
2016-12-14 14:03:26 -0600

    day 14 part 1

--- day14.py
+++ day14.py
@@ -12,17 +12,32 @@ class Calculator(object):
         self.hashes = {}
 
     def get_hash(self, index):
-        if index not in self.hashes:
-            hasher = hashlib.md5()
-            hasher.update((self.salt + str(index)).encode('utf-8'))
-            self.hashes[index] = hasher.hexdigest()
-        return self.hashes[index]
+        #if index not in self.hashes:
+        hasher = hashlib.md5()
+        hasher.update((self.salt + str(index)).encode('utf-8'))
+        return hasher.hexdigest()
+            #self.hashes[index] = hasher.hexdigest()
+        #return self.hashes[index]
 
 
 def solve1(data):
     triple_regex = re.compile(r'(.)\1\1')
     calc = Calculator(data)
-    return [(i, calc.get_hash(i), triple_regex.search(calc.get_hash(i))) for i in range(20)]
+    keys = []
+    index = -1
+    while len(keys) < 64:
+        index += 1
+        md5 = calc.get_hash(index)
+        match = triple_regex.search(md5)
+        if match:
+            subindex = index + 1
+            seq = match.group(1) * 5
+            if any(seq in calc.get_hash(subindex + i) for i in range(1000)):
+                print("found key {0} at index {1}".format(md5, index))
+                keys.append(md5)
+
+
+    return keys, index
 
 
 def solve2(data):
@@ -32,6 +47,6 @@ def solve2(data):
 if __name__ == '__main__':
 
     data = 'zpqevtbw'
-    data = 'abc'
+    #data = 'abc'
     pprint.pprint(solve1(data))
     pprint.pprint(solve2(data))
