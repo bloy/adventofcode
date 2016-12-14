cff6585a1b8786e9d8619157bac79659dc3acf35
Mike Bloy <mike@bloy.org>
2016-12-14 08:09:47 -0600

    day 14 prep

---
+++ day14.py
@@ -0,0 +1,16 @@
+#!/bin/env python3
+
+
+def solve1(data):
+    pass
+
+
+def solve2(data):
+    pass
+
+
+if __name__ == '__main__':
+
+    data = 'zpqevtbw'
+    print(solve1(data))
+    print(solve2(data))
