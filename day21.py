92e895a088b6b3c6a661e2587c6bc853e7988d31
Mike Bloy <mike@bloy.org>
2016-12-21 10:39:05 -0600

    day21 part 1

--- day21.py
+++ day21.py
@@ -1,40 +1,32 @@
 #!/usr/bin/env python3
 
 def swap_position(word, x, y):
-    letters = list(word)
-    letters[x], letters[y] = letters[y], letters[x]
-    word = "".join(letters)
+    word[x], word[y] = word[y], word[x]
     return word
 
 def swap_letter(word, x, y):
-    return swap_position(word, word.find(x), word.find(y))
+    return swap_position(word, word.index(x), word.index(y))
 
 def rotate_steps(word, direction, x):
-    x = x % len(word)
     if direction == 'right':
-        x = len(word) - x
-    if x == 0 or x == len(word):
-        return word
-    letters = list(word)
-    letters = letters[x:] + letters[:x]
-    word = "".join(letters)
-    return word
+        return word[-x:] + word[:-x]
+    else:
+        return word[x:] + word[:x]
 
 def rotate_pos(word, x):
-    index = word.find(x)
-    rotate = (index + 1) if index < 4 else (index + 2)
-    return rotate_steps(word, 'left', rotate)
+    index = word.index(x)
+    word = rotate_steps(word, 'right', 1)
+    word = rotate_steps(word, 'right', index)
+    if index >= 4:
+        word = rotate_steps(word, 'right', 1)
+    return word
 
 def reverse_pos(word, x, y):
-    letters = list(word)
-    letters[x:y+1] = reversed(letters[x:y+1])
-    word = "".join(letters)
+    word[x:y+1] = reversed(word[x:y+1])
     return word
 
 def move_pos(word, x, y):
-    letters = list(word)
-    letters[x:x+1], letters[y:y] = [], letters[x:x+1]
-    word = "".join(letters)
+    word[x:x+1], word[y:y] = [], word[x:x+1]
     return word
 
 def parse_line(line):
@@ -56,28 +48,29 @@ def parse_line(line):
 
 def solve(word, data):
     print(word)
+    word = list(word)
     for instruction in data:
-        print(instruction)
+        print(instruction[0].__name__, instruction[1:])
         word = instruction[0](word, *instruction[1:])
         print(word)
-    return word
+    return "".join(word)
 
 if __name__ == '__main__':
     with open('day21_input.txt') as f:
         data = [parse_line(line) for line in f.readlines() if line]
     word = 'abcdefgh'
 
-    data = [
-        (swap_position, 4, 0),
-        (swap_letter, 'd', 'b'),
-        (reverse_pos, 0, 4),
-        (rotate_steps, 'left', 1),
-        (move_pos, 1, 4),
-        (move_pos, 3, 0),
-        (rotate_pos, 'b'),
-        (rotate_pos, 'd'),
-    ]
-    word = 'abcde'
+    # data = [
+        # (swap_position, 4, 0),
+        # (swap_letter, 'd', 'b'),
+        # (reverse_pos, 0, 4),
+        # (rotate_steps, 'left', 1),
+        # (move_pos, 1, 4),
+        # (move_pos, 3, 0),
+        # (rotate_pos, 'b'),
+        # (rotate_pos, 'd'),
+    # ]
+    # word = 'abcde'
 
     word = solve(word, data)
     print(word)
