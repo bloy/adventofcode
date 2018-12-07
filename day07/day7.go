e47cb1a73a17cab7aec0f2f0ad8c70aa7ac1e603
Mike Bloy <mike.bloy@tdstelecom.com>
2018-12-07 12:05:02 -0600

    day 7 input parsed

--- day07/day7.go
+++ day07/day7.go
@@ -3,31 +3,45 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"regexp"
-	"strconv"
 	"strings"
 )
 
-func getInput() []string {
-	re := regexp.MustCompile(`^#(\d+) @ (\d+),(\d+): (\d+)x(\d+)$`)
+type depType map[string][]string
+
+func getInput() depType {
 	content, err := ioutil.ReadFile("input.txt")
 	if err != nil {
 		panic(err)
 	}
 	strs := strings.Split(strings.TrimSpace(string(content)), "\n")
-	return strs
+	deps := make(depType)
+	for _, str := range strs {
+		parts := strings.Split(str, " ")
+		after := parts[1]
+		before := parts[7]
+		if deps[after] == nil {
+			deps[after] = make([]string, 0, 1)
+		}
+		if deps[before] == nil {
+			deps[before] = make([]string, 0, 1)
+		}
+		deps[before] = append(deps[before], after)
+	}
+	return deps
 }
 
-func runPart1(input []string) {
+func runPart1(input depType) {
+	fmt.Println(input)
 	fmt.Println("part 1 ")
 }
 
-func runPart2(input []string) {
+func runPart2(input depType) {
 	fmt.Println("part 2 ")
 }
 
 func main() {
 	input := getInput()
 	runPart1(input)
+	input = getInput()
 	runPart2(input)
 }
