d1ef2b2966dd603e06838c6fc8bdada9b70c8965
Mike Bloy <mike@bloy.org>
2018-12-08 15:04:03 -0600

    Day 8 part 1 and 2
    Recursing for part 2, explicit stack for part 1, just to mess with
    things

--- day08/day8.go
+++ day08/day8.go
@@ -9,36 +9,87 @@ import (
 
 const testStr string = `2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2`
 
-func getInput() []int {
+type NodeType struct {
+	children []NodeType
+	metadata []int
+}
+
+func atoi(str string) int {
+	i, err := strconv.Atoi(str)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func readNode(strs []string) (node NodeType, remaining []string) {
+	var numKids, numMeta int
+
+	numKids = atoi(strs[0])
+	numMeta = atoi(strs[1])
+	node.children = make([]NodeType, numKids)
+	node.metadata = make([]int, numMeta)
+	remaining = strs[2:]
+	for i := 0; i < numKids; i++ {
+		node.children[i], remaining = readNode(remaining)
+	}
+	for i := 0; i < numMeta; i++ {
+		node.metadata[i] = atoi(remaining[i])
+	}
+	remaining = remaining[numMeta:]
+	return node, remaining
+}
+
+func getInput() NodeType {
 	content, err := ioutil.ReadFile("input.txt")
 	if err != nil {
 		panic(err)
 	}
 	str := string(content)
-	str = testStr
+	//str = testStr
 	strs := strings.Split(strings.TrimSpace(str), " ")
-	var intList []int = []int{}
-	for _, str := range strs {
-		if i, err := strconv.Atoi(str); err == nil {
-			intList = append(intList, i)
-		} else {
-			panic(err)
+	topNode, _ := readNode(strs)
+	return topNode
+}
+
+func runPart1(input NodeType) {
+	nodeStack := []NodeType{}
+	var sum int
+	nodeStack = append(nodeStack, input)
+	for len(nodeStack) > 0 {
+		node := nodeStack[0]
+		nodeStack = nodeStack[1:]
+		for _, meta := range node.metadata {
+			sum += meta
+		}
+		for _, child := range node.children {
+			nodeStack = append([]NodeType{child}, nodeStack...)
 		}
 	}
-	return intList
+
+	fmt.Println("part 1", sum)
 }
 
-func runPart1(input []int) {
-	fmt.Println("part 1", " n/a")
+func nodeValue(node NodeType) int {
+	var sum int
+	for _, meta := range node.metadata {
+		if len(node.children) == 0 {
+			sum += meta
+		} else {
+			if meta > 0 && meta <= len(node.children) {
+				sum += nodeValue(node.children[meta-1])
+			}
+		}
+	}
+	return sum
 }
 
-func runPart2(input []int) {
-	fmt.Println("part 2", " n/a")
+func runPart2(input NodeType) {
+	fmt.Println("part 2", nodeValue(input))
 }
 
 func main() {
 	input := getInput()
 	runPart1(input)
-	input = getInput()
 	runPart2(input)
 }
