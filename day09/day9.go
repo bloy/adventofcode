3eedacbb14b5ee6d568b8be152e842ae8411d32b
Mike Bloy <mike@bloy.org>
2018-12-09 15:31:47 -0600

    day9

--- day09/day9.go
+++ day09/day9.go
@@ -1,42 +1,53 @@
 package main
 
 import (
+	"container/ring"
 	"fmt"
-	"io/ioutil"
-	"strconv"
-	"strings"
 )
 
-const testStr string = ``
-
-func atoi(str string) int {
-	i, err := strconv.Atoi(str)
-	if err != nil {
-		panic(err)
+func runPart1(playerCount, maxMarble int) {
+	scores := make([]int, playerCount)
+	current := ring.New(1)
+	current.Value = 0
+	player := 0
+	for marble := 1; marble <= maxMarble; marble++ {
+		if marble%23 == 0 {
+			scores[player] += marble
+			removed := current
+			for i := 0; i < 7; i++ {
+				removed = removed.Prev()
+			}
+			scores[player] += removed.Value.(int)
+			current = removed.Next()
+			removed.Prev().Link(removed.Next())
+		} else {
+			after := current.Next()
+			insert := ring.New(1)
+			insert.Value = marble
+			after.Link(insert)
+			current = insert
+		}
+		//fmt.Printf("[%3d] %d", player, current.Value)
+		//for node := current.Next(); node != current; node = node.Next() {
+		//fmt.Printf(" %d", node.Value)
+		//}
+		//fmt.Println("")
+		player = (player + 1) % playerCount
 	}
-	return i
-}
-
-func getInput() string {
-	content, err := ioutil.ReadFile("input.txt")
-	if err != nil {
-		panic(err)
+	max := 0
+	for i := range scores {
+		if scores[i] > max {
+			max = scores[i]
+		}
 	}
-	str := string(content)
-	str = testStr
-	return str
-}
-
-func runPart1(input string) {
-	fmt.Println("part 1", input)
-}
-
-func runPart2(input string) {
-	fmt.Println("part 2")
+	fmt.Println("max score:", max)
 }
 
 func main() {
-	input := getInput()
-	runPart1(input)
-	runPart2(input)
+	const playerCount int = 403
+	const maxMarble int = 71920
+	//const playerCount int = 10
+	//const maxMarble int = 1618
+	runPart1(playerCount, maxMarble)
+	runPart1(playerCount, 100*maxMarble)
 }
