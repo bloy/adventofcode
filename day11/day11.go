f13ee4d3be768a0c9d4e9956e697c9f77a0d1fcc
Mike Bloy <mike.bloy@tdstelecom.com>
2018-12-11 15:30:54 -0600

    day11

--- day11/day11.go
+++ day11/day11.go
@@ -47,34 +47,56 @@ func greatestPower1(serial, gridSize int) {
 }
 
 func greatestPower2(serial, gridSize int) {
-	var cells map[Point]int = make(map[Point]int, gridSize*gridSize)
-	var maxPower int = -9999
+	var subGrids []int = make([]int, gridSize*gridSize)
+	var maxPower int
 	var maxPowerPoint Point
 	var maxPowerSize int
-	// TODO: precompute cell values
+	for x := 1; x <= gridSize; x++ {
+		rackID := x + 10
+		columnTotal := 0
+		for y := 1; y <= gridSize; y++ {
+			adjacentBoxTotal := 0
+			if x > 1 {
+				adjacentBoxTotal = subGrids[(y-1)*gridSize+(x-2)]
+			}
+			level := ((rackID * y) + serial) * rackID
+			level = level / 100
+			level = level % 10
+			level = level - 5
+			columnTotal += level
+			subGrids[(y-1)*gridSize+(x-1)] = adjacentBoxTotal + columnTotal
+		}
+	}
+
 	// TODO: precompute 1,1-x,y subgrid values for all subgrids
 	// subgrid calc is summed area(x2,y2) - summed area(x2,y1) - summed area(x1,y2) + summed area(x1,y1)
 	for size := 1; size <= gridSize; size++ {
-		fmt.Println(size)
 		for y := 1; y <= gridSize-size+1; y++ {
 			for x := 1; x <= gridSize-size+1; x += 1 {
-				power := 0
-				for suby := y; suby < y+size; suby++ {
-					for subx := x; subx < x+size; subx++ {
-						subcell := Point{subx, suby}
-						if value, ok := cells[subcell]; ok == true {
-							power += value
-						} else {
-							rackID := subcell.x + 10
-							level := ((rackID * subcell.y) + serial) * rackID
-							level = level / 100
-							level = level % 10
-							level = level - 5
-							cells[subcell] = level
-							power += level
-						}
-					}
+				// quadrants:
+				//   Q1 Q2
+				//   Q3 Q4
+				// want the Q4 total
+				// have the total of all, Q1, Q2 + Q1 (Q12), and Q3 + Q1 (Q13)
+				// Q4 = ALL - Q13 - Q12 + Q1
+				x1 := x - 2
+				x2 := x - 1 + size - 1
+				y1 := y - 2
+				y2 := y - 1 + size - 1
+				q1Total := 0
+				q12Total := 0
+				q13Total := 0
+				allTotal := subGrids[y2*gridSize+x2]
+				if x1 >= 0 && y1 >= 0 {
+					q1Total = subGrids[y1*gridSize+x1]
+					q12Total = subGrids[y1*gridSize+x2]
+					q13Total = subGrids[y2*gridSize+x1]
+				} else if x1 >= 0 {
+					q13Total = subGrids[y2*gridSize+x1]
+				} else if y1 >= 0 {
+					q12Total = subGrids[y1*gridSize+x2]
 				}
+				power := allTotal - q12Total - q13Total + q1Total
 				if power > maxPower {
 					maxPower = power
 					maxPowerPoint = Point{x, y}
