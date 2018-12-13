e3651499896aaf34d0072972a502eeba13362265
Mike Bloy <mike@bloy.org>
2018-12-13 16:57:34 -0600

    day 12 part 1

--- day12/day12.go
+++ day12/day12.go
@@ -53,95 +53,71 @@ func getInput() (initial *list.List, rules map[string]bool) {
 	}
 	for _, str := range strings.Split(rulesStrs, "\n") {
 		parts := strings.Split(str, " ")
-		rules[parts[0]] = rules[parts[2]]
+		if parts[2] == "#" {
+			rules[parts[0]] = true
+		} else {
+			rules[parts[0]] = false
+		}
 	}
 	return
 }
 
+func printPots(firstPot int, state *list.List) {
+	fmt.Printf("%d ", firstPot)
+	for pot := state.Front(); pot != nil; pot = pot.Next() {
+		if pot.Value.(bool) {
+			fmt.Print("#")
+		} else {
+			fmt.Print(".")
+		}
+	}
+	fmt.Println("")
+}
+
 func runPart1(state *list.List, rules map[string]bool) {
 	firstPot := 0
-	for i := 0; i < 20; i++ {
-		nextState := list.New()
-		minus1pots := []string{".", ".", ".", ".", "."}
-		minus2pots := []string{".", ".", ".", ".", "."}
-		if state.Front() != nil {
-			if state.Front().Value.(bool) {
-				minus1pots[3] = "#"
-				minus2pots[4] = "#"
-			}
-			if state.Front().Next() != nil && state.Front().Next().Value.(bool) {
-				minus1pots[4] = "#"
-			}
+	pots := list.New()
+	pots.PushBackList(state)
+	printPots(firstPot, pots)
+	for generation := 0; generation < 20; generation++ {
+		nextPots := list.New()
+		for extra := 0; extra < 4; extra++ {
+			pots.PushFront(false)
+			pots.PushBack(false)
 		}
-		minus1check := strings.Join(minus1pots, "")
-		minus2check := strings.Join(minus2pots, "")
-		if rules[minus1check] == true && rules[minus2check] == true {
-			nextState.PushFront(true)
-			nextState.PushFront(true)
-			firstPot -= 2
-		} else if rules[minus1check] == true {
-			nextState.PushFront(true)
-			firstPot -= 1
-		} else if rules[minus2check] == true {
-			nextState.PushFront(false)
-			nextState.PushFront(true)
-			firstPot -= 2
-		}
-		for e := state.Front(); e != nil; e = e.Next() {
-			pots := []string{".", ".", ".", ".", "."}
-			if e.Prev() != nil {
-				if e.Prev().Value.(bool) {
-					pots[1] = "#"
+		potNum := firstPot - 2
+		filledPot := false
+		for pot := pots.Front().Next().Next(); pot.Next().Next() != nil; pot = pot.Next() {
+			checks := make([]string, 0, 5)
+			rangePot := pot.Prev().Prev()
+			for i := 0; i < 5; i++ {
+				var checkchar string
+				if rangePot.Value.(bool) {
+					checkchar = "#"
+				} else {
+					checkchar = "."
 				}
-				if e.Prev().Prev() != nil && e.Prev().Prev().Value.(bool) {
-					pots[0] = "#"
+				checks = append(checks, checkchar)
+				rangePot = rangePot.Next()
+			}
+			checkStr := strings.Join(checks, "")
+			if rules[checkStr] == true {
+				nextPots.PushBack(true)
+				if !filledPot {
+					filledPot = true
+					firstPot = potNum
 				}
+			} else if filledPot { // previous filled pot and this one is not filled
+				nextPots.PushBack(false)
 			}
-			if e.Value.(bool) {
-				pots[2] = "#"
-			}
-			if e.Next() != nil {
-				if e.Next().Value.(bool) {
-					pots[3] = "#"
-				}
-				if e.Next().Next() != nil && e.Next().Next().Value.(bool) {
-					pots[4] = "#"
-				}
-			}
-			check := strings.Join(pots, "")
-			if rules[check] == true {
-				nextState.PushBack(true)
-			} else {
-				nextState.PushBack(false)
-			}
+			potNum++
 		}
-		plus1pots := []string{".", ".", ".", ".", "."}
-		plus2pots := []string{".", ".", ".", ".", "."}
-		if state.Back() != nil {
-			if state.Back().Value.(bool) {
-				plus1pots[1] = "#"
-				plus2pots[0] = "#"
-			}
-			if state.Back().Prev() != nil && state.Back().Prev().Value.(bool) {
-				plus1pots[0] = "#"
-			}
-		}
-		plus1check := strings.Join(plus1pots, "")
-		plus2check := strings.Join(plus2pots, "")
-		if rules[plus1check] == true && rules[plus2check] == true {
-			nextState.PushBack(true)
-			nextState.PushBack(true)
-		} else if rules[plus1check] == true {
-			nextState.PushBack(true)
-		} else if rules[plus2check] == true {
-			nextState.PushBack(false)
-			nextState.PushBack(true)
-		}
-		state = nextState
+		pots = nextPots
+		printPots(firstPot, pots)
 	}
 	sum := 0
 	num := firstPot
-	for e := state.Front(); e != nil; e = e.Next() {
+	for e := pots.Front(); e != nil; e = e.Next() {
 		if e.Value.(bool) {
 			sum += num
 		}
@@ -151,11 +127,63 @@ func runPart1(state *list.List, rules map[string]bool) {
 }
 
 func runPart2(state *list.List, rules map[string]bool) {
-	fmt.Println("part 2")
+	firstPot := 0
+	pots := list.New()
+	pots.PushBackList(state)
+	printPots(firstPot, pots)
+	for generation := 0; generation < 50000000000; generation++ {
+		if generation%100 == 0 {
+			var genf float32 = float32(generation)
+			var pct float32 = genf / 50000000000 * 100
+			fmt.Printf("gen %d - %0.2f percent\n", generation, pct)
+		}
+		nextPots := list.New()
+		for extra := 0; extra < 4; extra++ {
+			pots.PushFront(false)
+			pots.PushBack(false)
+		}
+		potNum := firstPot - 2
+		filledPot := false
+		for pot := pots.Front().Next().Next(); pot.Next().Next() != nil; pot = pot.Next() {
+			checks := make([]string, 0, 5)
+			rangePot := pot.Prev().Prev()
+			for i := 0; i < 5; i++ {
+				var checkchar string
+				if rangePot.Value.(bool) {
+					checkchar = "#"
+				} else {
+					checkchar = "."
+				}
+				checks = append(checks, checkchar)
+				rangePot = rangePot.Next()
+			}
+			checkStr := strings.Join(checks, "")
+			if rules[checkStr] == true {
+				nextPots.PushBack(true)
+				if !filledPot {
+					filledPot = true
+					firstPot = potNum
+				}
+			} else if filledPot { // previous filled pot and this one is not filled
+				nextPots.PushBack(false)
+			}
+			potNum++
+		}
+		pots = nextPots
+	}
+	sum := 0
+	num := firstPot
+	for e := pots.Front(); e != nil; e = e.Next() {
+		if e.Value.(bool) {
+			sum += num
+		}
+		num++
+	}
+	fmt.Println("part 2", firstPot, sum)
 }
 
 func main() {
 	state, rules := getInput()
 	runPart1(state, rules)
-	runPart2(state, rules)
+	//runPart2(state, rules)
 }
