696a826fbcba2a2b51f93aa11cff992e234c8c38
Mike Bloy <mike@bloy.org>
2018-12-17 00:32:37 -0600

    skeleton for day 16 part 2

--- day16/main.go
+++ day16/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"container/list"
 	"fmt"
 )
 
 func main() {
 	samples := ParseSamples(SampleStr)
 	doPart1(samples)
+	doPart2(samples)
+}
 
+func matchSample(f OpcodeFunc, sample *Sample) bool {
+	tmp := regCopy(sample.Before)
+	f(sample.Input1, sample.Input2, sample.Output, &tmp)
+	if tmp == sample.After {
+		return true
+	} else {
+		return false
+	}
 }
 
 func doPart1(samples []*Sample) {
@@ -15,9 +26,7 @@ func doPart1(samples []*Sample) {
 	for _, sample := range samples {
 		var count int = 0
 		for _, f := range funcs {
-			tmp := regCopy(sample.Before)
-			f(sample.Input1, sample.Input2, sample.Output, &tmp)
-			if tmp == sample.After {
+			if matchSample(f, sample) {
 				count++
 			}
 			if count >= 3 {
@@ -29,4 +38,43 @@ func doPart1(samples []*Sample) {
 		}
 	}
 	fmt.Printf("%d of %d samples match 3 more more opcodes\n", sampleCount, len(samples))
+}
+
+func doPart2(samples []*Sample) {
+	var gather map[int][]*Sample = make(map[int][]*Sample)
+	for _, sample := range samples {
+		code := sample.Opcode
+		if gather[code] == nil {
+			gather[code] = make([]*Sample, 0, 1)
+		}
+		gather[code] = append(gather[code], sample)
+	}
+	var codeMap map[int]OpcodeFunc = make(map[int]OpcodeFunc, len(gather))
+	var possibles map[int]*list.List = make(map[int]*list.List)
+	for code := range gather {
+		fmt.Printf("code: %d, samples: %d\n", code, len(gather[code]))
+		possibles[code] = list.New()
+		for _, f := range funcs {
+			matched := true
+			for _, s := range gather[code] {
+				if !matchSample(f, s) {
+					matched = false
+					break
+				}
+			}
+			if matched {
+				fmt.Printf(" found! - %v\n", f)
+				possibles[code].PushBack(f)
+			}
+		}
+		if possibles[code].Len() == 1 {
+			codeMap[code] = possibles[code].Front().Value.(OpcodeFunc)
+			delete(possibles, code)
+		}
+	}
+	// TODO: while the length of codeMap < length of gather, look for a
+	// possible with a len of 1. Add it to codemap, remove it from possibles,
+	// repeat until done
+	// TODO: parse code
+	// TODO: run code
 }
