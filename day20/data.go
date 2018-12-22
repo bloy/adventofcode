55b73c416af79766e14b8b9e5c1fc9ee0f07c407
Mike Bloy <mike@bloy.org>
2018-12-22 16:45:27 -0600

    slightly broken day 20 but it works

--- day20/data.go
+++ day20/data.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"container/list"
+	"fmt"
+	"io/ioutil"
+)
+
 func getInput() string {
 	content, err := ioutil.ReadFile("input.txt")
 	if err != nil {
@@ -8,12 +14,42 @@ func getInput() string {
 	return string(content)
 }
 
+func ParseStr(str string) *Base {
+	var b *Base = NewBase()
+	var cur *Room
+	stack := list.New()
+	for _, ch := range str {
+		switch ch {
+		case '^':
+			cur = b.StartRoom
+		case 'N':
+			cur = b.AddRoom(NORTH, cur)
+		case 'E':
+			cur = b.AddRoom(EAST, cur)
+		case 'S':
+			cur = b.AddRoom(SOUTH, cur)
+		case 'W':
+			cur = b.AddRoom(WEST, cur)
+		case '(':
+			stack.PushFront(cur)
+		case ')':
+			stack.Remove(stack.Front())
+		case '|':
+			cur = stack.Front().Value.(*Room)
+		case '$':
+			fmt.Println("Done with string")
+		}
+	}
+	return b
+}
+
 var inputStr string = getInput()
 
 const (
-	testStr1 string = `^WNE$`
-	testStr2 string = `^ENWWW(NEEE|SSE(EE|N))$`
-	testStr3 string = `^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$`
-	testStr4 string = `^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$`
-	testStr5 string = `^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$`
+	testStr1  string = `^WNE$`
+	testStr2a string = `^N(E|W)N$`
+	testStr2  string = `^ENWWW(NEEE|SSE(EE|N))$`
+	testStr3  string = `^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$`
+	testStr4  string = `^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$`
+	testStr5  string = `^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$`
 )
--- day20/types.go
+++ day20/types.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 type Point struct {
 	X, Y int
 }
@@ -18,10 +22,11 @@ type Room struct {
 	Position Point
 }
 
-func NewRoom(x, y int) *Room {
+func NewRoom(p Point) *Room {
 	var r *Room = &Room{}
 	r.Doors = make(map[Direction]*Room)
-	r.Point = Point{x, y}
+	r.Position = Point{p.X, p.Y}
+	return r
 }
 
 type Base struct {
@@ -36,6 +41,47 @@ func NewBase() *Base {
 	b.Rooms = make(map[Point]*Room)
 	b.MinPos = Point{0, 0}
 	b.MaxPos = Point{0, 0}
-	b.StartRoom = NewRoom(0, 0)
+	b.StartRoom = NewRoom(Point{0, 0})
 	b.Rooms[b.StartRoom.Position] = b.StartRoom
+	return b
+}
+
+func (b *Base) AddRoom(dir Direction, fromRoom *Room) (toRoom *Room) {
+	newPoint := Point{fromRoom.Position.X, fromRoom.Position.Y}
+	var reverseDir Direction
+	switch dir {
+	case NORTH:
+		reverseDir = SOUTH
+		newPoint.Y = newPoint.Y - 1
+	case EAST:
+		reverseDir = WEST
+		newPoint.X = newPoint.X + 1
+	case SOUTH:
+		reverseDir = NORTH
+		newPoint.Y = newPoint.Y + 1
+	case WEST:
+		reverseDir = EAST
+		newPoint.X = newPoint.X - 1
+	}
+	if newPoint.X < b.MinPos.X {
+		b.MinPos.X = newPoint.X
+	}
+	if newPoint.X > b.MaxPos.X {
+		b.MaxPos.X = newPoint.X
+	}
+	if newPoint.Y < b.MinPos.Y {
+		b.MinPos.Y = newPoint.Y
+	}
+	if newPoint.Y > b.MaxPos.Y {
+		b.MaxPos.Y = newPoint.Y
+	}
+	toRoom = b.Rooms[newPoint]
+	if toRoom == nil {
+		fmt.Println("new room at", newPoint)
+		toRoom = NewRoom(newPoint)
+		b.Rooms[newPoint] = toRoom
+	}
+	fromRoom.Doors[dir] = toRoom
+	toRoom.Doors[reverseDir] = fromRoom
+	return toRoom
 }
