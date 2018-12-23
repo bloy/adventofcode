2d2ef6c297860f81abba6952fd6a0e9ffb666c7d
Mike Bloy <mike@bloy.org>
2018-12-23 00:22:53 -0600

    day 22 try to implement priority queue

--- day22/types.go
+++ day22/types.go
@@ -8,6 +8,13 @@ type Point struct {
 	X, Y int
 }
 
+var NORTH Point = Point{0, -1}
+var EAST Point = Point{1, 0}
+var SOUTH Point = Point{0, 1}
+var WEST Point = Point{-1, 0}
+
+var DIRECTIONS [4]Point = [4]Point{NORTH, EAST, SOUTH, WEST}
+
 type RegionType int8
 
 const (
@@ -16,6 +23,22 @@ const (
 	WET
 	NARROW
 )
+
+type Item int8
+
+const (
+	NEITHER Item = iota
+	TORCH
+	CLIMB
+)
+
+var EQUIPMENT [3]Item = [3]Item{NEITHER, TORCH, CLIMB}
+
+var ItemNotAllowed map[RegionType]Item = map[RegionType]Item{
+	ROCKY:  NEITHER,
+	WET:    TORCH,
+	NARROW: CLIMB,
+}
 
 type Region struct {
 	Point
@@ -105,4 +128,52 @@ func (c *Cave) String() string {
 		str.WriteString("\n")
 	}
 	return str.String()
+}
+
+type Node struct {
+	Point
+	Minutes  int
+	Equipped Item
+	index    int
+}
+
+type SeenNode struct {
+	Point
+	Equipped Item
+}
+
+type PriorityQueue []*Node
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	if pq[i].Minutes == pq[j].Minutes {
+		if pq[i].X == pq[j].X {
+			return pq[i].Y < pq[j].Y
+		}
+		return pq[i].X < pq[j].X
+	}
+	return pq[i].Minutes == pq[j].Minutes
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	n := len(*pq)
+	node := x.(*Node)
+	node.index = n
+	*pq = append(*pq, node)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	item.index = -1
+	*pq = old[0 : n-1]
+	return item
 }
