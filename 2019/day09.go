70b0c275192c29ce58d8d7c9ec5da91fca9da2bb
Mike Bloy <mike@bloy.org>
2019-12-08 23:24:47 -0600

    day 09 will need to revamp intcode

---
+++ 2019/day09.go
@@ -0,0 +1,20 @@
+package main
+
+import "bufio"
+
+func init() {
+	AddSolution(9, solveDay9)
+
+}
+func solveDay9(pr *PuzzleRun) {
+	scanner := bufio.NewScanner(pr.InFile)
+	program := ""
+	for scanner.Scan() {
+		program = scanner.Text()
+	}
+	if err := scanner.Err(); err != nil {
+		pr.logger.Fatal(err)
+	}
+	pr.ReportLoad()
+
+}
